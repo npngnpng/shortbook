e1c1442e07fb2a87dac119088702f6d1faae0722
길근우 <kesksjxhx@gmail.com>
2024-03-22 09:22:27 +0900

    🗑️ :: 테스트 x

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,9 +37,6 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
 }
 
 dependencyManagement {
@@ -63,8 +60,4 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "17"
     }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
